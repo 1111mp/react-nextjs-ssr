345cac7df82588665067c59eeacab23cec5b88f1
张逸凡 <zhangyifan@zhangyifandeMacBook-Pro.local>
2019-11-06 21:11:04 +0800

    fix css and sass

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
+module.exports = withPlugins([[withCSS, {
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
+  devIndicators: {
+    autoPrerender: false,
+  }
+}], [withSass, {
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
   }
-})
+}]])
